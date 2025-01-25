c736daeda3918b9d13047c7349103c14a8b722a1
gordidev <149323382+gordidev@users.noreply.github.com>
2025-01-25 23:40:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+let index = 0 // Se establece el indice del array en 0
+let selected = 0 // Respuesta selecionada
+let points = 0 // Puntos
+const resetCounter = 30 // Segundos del contador
+let counter = resetCounter // Segundos que le quedan al contador para llegar a 0
+let shuffleQuests = [] // Array donde se guardaran las preguntas desordenadas
+let intervalo = null; // Variable para almacenar el intervalo actual
+
+const questsDiv = document.querySelector("#quests") // Obtenemos el div donde se mostraran las preguntas
+const counterElement = document.querySelector("#contadorid") // Obtenemos el H1 del contador
+
+//fetch para obtener un array de datos desde una API
+fetch('api.json')
+  .then(response => response.json())
+  .then(data => {
+    // Desordenar el array de datos
+    shuffleQuests = desordenarArray(data);
+    //console.log(shuffleQuests);
+  })
+  .catch(error => console.error('Error al obtener los datos:', error));
+
+// Función para desordenar el array
+function desordenarArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // Generar índice aleatorio
+      [array[i], array[j]] = [array[j], array[i]]; // Intercambiar los elementos
+    }
+    return array;
+  }
+function restar(){
+    clearInterval(intervalo)
+    counterElement.innerHTML = counter
+    intervalo = setInterval(contadorfunc, 1000)
+}
+
+function contadorfunc(){
+    if(counter == 0){
+        clearInterval(intervalo); // Detener el intervalo al llegar a 0
+        statusQuest(0)
+    }else{
+    counter--
+    counterElement.innerHTML = counter
+    console.log(counter)
+    }
+    
+}
+function statusQuest(stat){
+    clearInterval(intervalo)
+    counterElement.style.visibility = "hidden"
+    if(stat == 0){
+        questsDiv.innerHTML = `<h1 class="text-danger">Fallaste la respuesta era la ${shuffleQuests[index].correct}</h1><h1>Puntos: ${points}</h1><h1>Preguntas: ${index+1}/${shuffleQuests.length}</h1><input type="button" value="Siguiente" onclick="loadQuest()">`
+    }else{
+        points = points+counter
+        questsDiv.innerHTML = `<h1 class="text-success">Acertaste la respuesta era la ${shuffleQuests[index].correct}</h1><h1>Puntos: ${points}</h1><h1>Preguntas: ${index+1}/${shuffleQuests.length}</h1><input type="button" value="Siguiente" onclick="loadQuest()">`
+    }
+}
+
+function button(answer){
+    selected = answer
+    if(selected == shuffleQuests[index].correct){
+        statusQuest(1)
+        console.log("acertaste");
+    }else{
+        console.log("fallaste");
+        statusQuest(0)
+    }
+    index++;
+    //loadQuest()
+}
+
+function loadQuest(){
+    counter=resetCounter
+    if(index == shuffleQuests.length){
+        questsDiv.innerHTML = `<h1>Terminaste</h1><h1>Puntos: ${points}/300</h1><button class="btn btn-success" onclick="location.reload()">Volver a inicio</button>`
+    }else{
+        questsDiv.innerHTML = `
+        <h1>${index+1}.-${shuffleQuests[index].title}</h1>
+        <input type="button" value="${shuffleQuests[index].answer1}" onclick="button(1)">
+        <input type="button" value="${shuffleQuests[index].answer2}" onclick="button(2)">
+        <input type="button" value="${shuffleQuests[index].answer3}" onclick="button(3)">
+        `
+        restar()
+        counterElement.style.visibility = "visible"
+    }
+}
